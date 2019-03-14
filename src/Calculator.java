3d892e17eba7b09d91b0d67db601ff4ca87ca6c6
mm <mm@218.38.137.28>
2019-03-15 04:12:27 +0900

    계산기 기본 기능 구현 완

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,31 +8,46 @@ public class Calculator {
 		int first = scanner.nextInt();
 		System.out.println(first);
 
-		System.out.print("사칙연산 기호 : ");
-		String symbol = scanner.next();
-		System.out.println(symbol);
+		int result = first;
+		
+		while (true) {
+			System.out.print("사칙연산 기호 : ");
+			String symbol = scanner.next();
+			System.out.println(symbol);
+			
+			if (symbol.equals("quit")) {
+				// TODO 최종 실행 결과 값 출력
+				System.out.println("최종 결과 값 : " + result);
+				break;
+			}
 
-		// TODO 두 번째 값을 입력 받아 저장
-		System.out.print("두 번째 입력 값 : ");
-		int second = scanner.nextInt();
-		System.out.println(second);
+			// TODO 두 번째 값을 입력 받아 저장
+			System.out.print("두 번째 입력 값 : ");
+			int second = scanner.nextInt();
+			System.out.println(second);
 
-		// TODO입력 받은 두 값에 대한 사칙연산을 계산해 출력
-		if (symbol.equals("+")) {
-			// TODO 덧셈 계산 후 출력
-			System.out.println("덧셈 : " + (first + second));
-		} else if (symbol.equals("-")) {
-			// TODO 뺄셈 계산 후 출력
-			System.out.println("뺄셈 : " + (first - second));
-		} else if ("*".equals(symbol)) {
-			// TODO 곱셈 게산 후 출력
-			System.out.println("곱셈 : " + (first * second));
-		} else if ("/".equals(symbol)) {
-			// TODO 나눗셈 계산 후 출력
-			System.out.println("나눗셈 : " + (first / second));
-		} else {
-			System.out.println("사칙연산에 해당하지 않는 값을 입력했습니다.");
+			// TODO 입력 받은 두 값에 대한 사칙연산을 계산해 출력
+			
+			if (symbol.equals("+")) {
+				// TODO 덧셈 계산 후 출력
+				result = result + second;
+				System.out.println("덧셈 : " + result);
+			} else if (symbol.equals("-")) {
+				// TODO 뺄셈 계산 후 출력
+				result = result - second;
+				System.out.println("뺄셈 : " + result);
+			} else if (symbol.equals("*")) {
+				// TODO 곱셈 게산 후 출력
+				result = result * second;
+				System.out.println("곱셈 : " + result);
+			} else if (symbol.equals("/")) {
+				// TODO 나눗셈 계산 후 출력
+				result = result / second;
+				System.out.println("나눗셈 : " + result);
+			} else {
+				System.out.println("사칙연산에 해당하지 않는 값을 입력했습니다.");
+			}
+
 		}
-
 	}
 }
