bc4c7e09e6e3b4e5c20f7200c2dcbbbd435880e7
mm <mm@218.38.137.28>
2019-03-15 04:32:21 +0900

    메소드를 추가해 로직을 분리

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,53 +1,69 @@
 import java.util.Scanner;
 
 public class Calculator {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-
+	static int getFirstValue(Scanner scanner) {
 		System.out.print("첫 번째 입력 값 : ");
 		int first = scanner.nextInt();
 		System.out.println(first);
-
+		return first;
+	}
+	
+	static String getSymbol(Scanner scanner) {
+		System.out.print("사칙연산 기호 : ");
+		String symbol = scanner.next();
+		System.out.println(symbol);
+		return symbol;
+	}
+	
+	static int getSecondValue(Scanner scanner) {
+		System.out.print("두 번째 입력 값 : ");
+		int second = scanner.nextInt();
+		System.out.println(second);
+		return second;
+	}
+	
+	static int calculate(int first, String symbol, int second) {
+		int result = 0;
+		if (symbol.equals("+")) {
+			result = first + second;
+			System.out.println("덧셈 : " + result);
+		} else if (symbol.equals("-")) {
+			result = first - second;
+			System.out.println("뺄셈 : " + result);
+		} else if (symbol.equals("*")) {
+			result = first * second;
+			System.out.println("곱셈 : " + result);
+		} else if (symbol.equals("/")) {
+			result = first / second;
+			System.out.println("나눗셈 : " + result);
+		} else {
+			System.out.println("사칙연산에 해당하지 않는 값을 입력했습니다.");
+		}
+		return result;
+	}
+	
+	static void print(int result) {
+		System.out.println("최종 결과 값 : " + result);
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int first = getFirstValue(scanner);
+		
 		int result = first;
 		
 		while (true) {
-			System.out.print("사칙연산 기호 : ");
-			String symbol = scanner.next();
-			System.out.println(symbol);
+			String symbol = getSymbol(scanner);
 			
 			if (symbol.equals("quit")) {
-				// TODO 최종 실행 결과 값 출력
-				System.out.println("최종 결과 값 : " + result);
+				print(result);
 				break;
 			}
 
-			// TODO 두 번째 값을 입력 받아 저장
-			System.out.print("두 번째 입력 값 : ");
-			int second = scanner.nextInt();
-			System.out.println(second);
+			int second = getSecondValue(scanner);
 
-			// TODO 입력 받은 두 값에 대한 사칙연산을 계산해 출력
-			
-			if (symbol.equals("+")) {
-				// TODO 덧셈 계산 후 출력
-				result = result + second;
-				System.out.println("덧셈 : " + result);
-			} else if (symbol.equals("-")) {
-				// TODO 뺄셈 계산 후 출력
-				result = result - second;
-				System.out.println("뺄셈 : " + result);
-			} else if (symbol.equals("*")) {
-				// TODO 곱셈 게산 후 출력
-				result = result * second;
-				System.out.println("곱셈 : " + result);
-			} else if (symbol.equals("/")) {
-				// TODO 나눗셈 계산 후 출력
-				result = result / second;
-				System.out.println("나눗셈 : " + result);
-			} else {
-				System.out.println("사칙연산에 해당하지 않는 값을 입력했습니다.");
-			}
-
+			result = calculate(result, symbol, second);
 		}
 	}
 }
